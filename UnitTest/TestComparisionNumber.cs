f885c697a8f225dc1fd861b3e72e9b089b9ce556
Dan Milkov <danielmilkov@hotmail.com>
2017-05-26 23:12:52 +0500

    Добавлен проект с тестами

---
+++ UnitTest/TestComparisionNumber.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCows.Test
+{
+    [TestClass]
+    public class TestComparisionNumber
+    {
+        public void CountBullsCows(int[] e, int[] a, int[] expected)
+        {
+            var result = Program.ComparisonNumber(e, a);
+            for (int i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], result[i]);
+        }
+
+        [TestMethod]
+        public void B1C1()
+        {
+            CountBullsCows(new int[] { 1, 2, 3, 4 }, new int[] { 2, 7, 3, 5 }, new int[] { 1, 1 });
+        }
+
+        [TestMethod]
+        public void B4C0()
+        {
+            CountBullsCows(new int[] { 7, 4, 1, 9 }, new int[] { 7, 4, 1, 9 }, new int[] { 4, 0 });
+        }
+
+        [TestMethod]
+        public void B0C4()
+        {
+            CountBullsCows(new int[] { 3, 2, 7, 8 }, new int[] { 8, 3, 2, 7 }, new int[] { 0, 4 });
+        }
+
+        [TestMethod]
+        public void B2C2()
+        {
+            CountBullsCows(new int[] { 2, 8, 5, 3 }, new int[] { 2, 8, 3, 5 }, new int[] { 2, 2 });
+        }
+
+        [TestMethod]
+        public void B3C0()
+        {
+            CountBullsCows(new int[] { 9, 8, 7, 4 }, new int[] { 9, 8, 7, 5 }, new int[] { 3, 0 });
+        }
+
+        [TestMethod]
+        public void B0C3()
+        {
+            CountBullsCows(new int[] { 9, 8, 7, 4 }, new int[] { 4, 9, 8, 1 }, new int[] { 0, 3 });
+        }
+    }
+}
---
+++ UnitTest/TestInputValidation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BullsAndCows.Test
+{
+    [TestClass]
+    public class TestInputValidation
+    {
+        public void Validation(string input, bool expected)
+        {
+            var result = Program.InputValidation(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void NotNumber()
+        {
+            Validation("1a34", false);
+            Validation("dsfsdr", false);
+        }
+
+        [TestMethod]
+        public void MoreChar()
+        {
+            Validation("16435", false);
+        }
+
+        [TestMethod]
+        public void LessChar()
+        {
+            Validation("134", false);
+        }
+
+        [TestMethod]
+        public void Space()
+        {
+            Validation("1  4 3 2", false);
+        }
+
+        [TestMethod]
+        public void Clone()
+        {
+            Validation("1232", false);
+        }
+
+        [TestMethod]
+        public void RightInput()
+        {
+            Validation("1234", true);
+        }
+    }
+}
